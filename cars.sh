c19b33bf7740e217d8cdde81442bffb4c37ac279
Christian Ton-That <69617940+chriswonton@users.noreply.github.com>
2020-12-11 10:28:09 -0800

    Update cars.sh

--- cars.sh
+++ cars.sh
@@ -5,30 +5,34 @@
 echo "type 1 to enter a new car"
 echo "type 2 to display the list of cars"
 echo "type 3 to quit and exit the program"
-read input
+input = "0"
+read -r input
 
-while [ "$input" -lt 3 ]
+while [ "$input" -le 3 ]
+read -r input
 do
-	if [ "$input" -eq 2 ]
-	then
-		cat My_old_cars
-
-	elif [ "$input" -eq 1 ]
-	then
-		year=""
-		echo "year?"
-		read $year
-		make=""
-		echo "make?"
-		read $make
-		model=""
-		echo "model?"
-		colon=":"
-		newCar="$$year$colon$make$colon$model"
-		echo $newCar >> My_old_cars
-	else
-		echo "please enter 1, 2 or 3"
-	fi
-	read input
+	case "$input" in
+		"1")
+			year=""
+			echo "year?"
+			read $year
+		
+			make=""
+			echo "make?"
+			read $make
+		
+			model=""
+			echo "model?"
+			read $model
+		
+			colon=":"
+			newCar="$year$colon$make$colon$model"
+			echo $newCar >> My_old_cars
+			;;
+		"2")
+			cat My_old_cars;;
+		"3")
+			break;;
+	esac
 done
 echo "goodbye"
