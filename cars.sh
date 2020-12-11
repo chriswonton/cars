0fd3226840374d30fac861f395e7dfe04bad9f19
Christian Ton-That <69617940+chriswonton@users.noreply.github.com>
2020-12-10 22:31:02 -0800

    Add files via upload

---
+++ cars.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+# cars.sh
+# Christian Ton-That
+
+input=""
+echo "type 1 to enter a new car"
+echo "type 2 to display the list of cars"
+echo "type 3 to quit and exit the program"
+
+read $input
+while [ $input -ne 3 ]
+do
+	if [ $input -eq 1 ]
+	then
+		cat My_old_cars
+	elif [ $input -eq 2 ]
+	then
+		newCar=""
+		year=""
+		make=""
+		model=""
+		echo "year?"
+		read $year
+		echo "make?"
+		read $make
+		echo "model?"
+		read $model
+		newCar = year + ":" + make + ":" + model
+		echo newCar >> My_old_cars
+	else
+		echo "please enter 1, 2 or 3"
+	fi
+done
+echo "goodbye"
